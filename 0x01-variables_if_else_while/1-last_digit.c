140b4be9d9af83d57b7849d292b4af0a7dbabe76
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-06 23:18:06 +0000

    assign a random number to the variable two, learning c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,3 +1,30 @@
-#include <stedio>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d", n, n % 10);
+	if (n > 5)
+	{
+		printf("and is greater than 5\n");
+	}
+	else if (n < 5)
+	{
+		printf("and is less than 6 and not 0\n");
+	}
+	else
+	{
+		printf("and is 0\n");
+	}
+	return (0);
+}
