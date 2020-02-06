360a78019846cec382125529308109f8f1b876b2
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-06 02:12:54 +0000

    C program that prints, learning c rev1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 
 #include <stdio.h>
-main(void)
+int main(void)
 {
-	puts(\""Programming is like building a multilingual puzzle\n");
+	puts(\""Programming is like building a multilingual puzzle");
 	return 0;
 }
