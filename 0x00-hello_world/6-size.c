5a9437adb9417cb54752543a8032afeec1af8931
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-06 03:20:43 +0000

    program that prints the size of various types on the computer, learning c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	a = 16;
+
+	printf("Size of char data type : %d\n", sizeof(char));
+	printf("Size of int data type : %d\n", sizeof(int));
+	printf("Size of long int data type : %d\n", sizeof(long int));
+	printf("Size of long long int data type : %d\n", sizeof(long long int));
+	printf("Size of float data type : %d\n", sizeof(float));
+	return (0);
+}
