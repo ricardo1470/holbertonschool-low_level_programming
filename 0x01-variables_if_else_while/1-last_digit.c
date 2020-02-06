4abcc105a2a605acab7c07cad41400c1c40609f3
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-06 23:21:28 +0000

    assign a random number to the variable two, learning c rev

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,15 +16,15 @@ int main(void)
 	printf("Last digit of %d is %d", n, n % 10);
 	if (n > 5)
 	{
-		printf("and is greater than 5\n");
+		printf("and is greater than 5 \n");
 	}
 	else if (n < 5)
 	{
-		printf("and is less than 6 and not 0\n");
+		printf("and is less than 6 and not 0 \n");
 	}
 	else
 	{
-		printf("and is 0\n");
+		printf("and is 0 \n");
 	}
 	return (0);
 }
