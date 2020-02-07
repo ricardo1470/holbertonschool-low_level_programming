4d630d7b44389f4e13e7a1ed38f5dca85b2d567b
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-07 02:30:00 +0000

    prints all single digit numbers of base 10 starting from 0, learning c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desc: Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 48 ; ch <= 57 ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
