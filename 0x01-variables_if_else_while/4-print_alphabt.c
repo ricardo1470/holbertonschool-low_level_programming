2f0fb9bdafb63430618dd84ec41e81217f1bfdce
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-07 02:05:10 +0000

    prints the alphabet in lowercase, learning c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desc: Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		putchar ('\n');
+	}
+	return (0);
+}
