ede88eda316c29f27ef7686bd8b102442d6160fc
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-14 04:54:21 +0000

    The Fizz-Buzz test, learning c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Desc: Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%d ", i);
+		}
+		else
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz ");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz ");
+			}
+		}
+	}
+	return (0);
+}
