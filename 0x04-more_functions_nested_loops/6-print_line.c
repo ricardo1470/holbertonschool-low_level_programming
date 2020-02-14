5cfb16a9e7803e6dec61cc1f85fef2f35ff88565
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-14 00:19:38 +0000

    function that draws a straight line in the terminal, learning c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_line - Entry point
+ * Desc: Entry
+ @n: inv var
+ * Return: Always 0 (Success)
+ */
+ void print_line(int n)
+{
+	int c;
+
+	if (n > 0)
+	{
+		for (c = 0; c < n ; c++)
+		{
+			_putchar(95);
+		}
+	}
+	_putchar('\n');
+}
