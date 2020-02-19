1bdfd6051e3f599de5249df2cd49a3fde947c6f6
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-19 00:48:56 +0000

    function that reverses a string, learning c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * rev_string - Entry point
+ * Desc: Entry
+ *@s: pointer
+ * Return: Always 0 (Success)
+ */
+void rev_string(char *s)
+{
+	int i;
+	int j;
+	char k;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+	}
+	i--;
+	for (j = 0 ; j < i ; j++ , i--)
+	{
+	k = *(s + i);
+	*(s + i) = *(s + j);
+	*(s + j) = k;
+	}
+}
