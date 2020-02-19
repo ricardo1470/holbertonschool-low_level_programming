0b132438044eb6934369a6785c7586712900376b
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-19 18:28:27 +0000

    function that convert a string to an integer, learning c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * _atoi - Entry point
+ * Desc: Entry
+ *@s: pointer
+ * Return: Always 0 (Success)
+ */
+int _atoi(char *s)
+{
+	int nev = 1;
+	int i;
+	int num = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			nev *= -1;
+		}
+		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = (num * 10) + (s[i] - '0');
+				}
+		else if (num > 0)
+		{
+			break;
+		}
+	}
+	return (num * nev);
+}
