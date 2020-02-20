3ff29bee66aea51035a16e0b4bd3fbff3c4e33b5
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-20 23:37:41 +0000

    function that copies a string, learnin c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strcat - Entry point
+ * Desc: Entry
+ *@dest: pointer
+ *@src: pointer
+ *@n: var
+ * Return: Always 0 (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' && i < n ; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -5,5 +5,6 @@
 int _putchar(char c); /*_putchar*/
 char *_strcat(char *dest, char *src);/*prototype function that concatenates two strings*/
 char *_strncat(char *dest, char *src, int n);/*prototype function that concatenates two strings.*/
+char *_strncpy(char *dest, char *src, int n);/*prototype function that copies a string.*/
 
 #endif /* HOLBERTON_H */
