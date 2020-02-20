72722347fde4757e5bf4d6b84ce01f98f0bfb877
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-20 21:40:09 +0000

    function that concatenates two strings, learning c

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+int _putchar(char c); /*_putchar*/
+char *_strcat(char *dest, char *src);/*prototype function that concatenates two strings*/
+
+#endif /* HOLBERTON_H */
