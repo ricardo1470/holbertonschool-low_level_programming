f6a75caef768d8822fe4848e79af733eeb0639bf
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-20 21:43:11 +0000

    function that concatenates two strings, learning c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -17,8 +17,8 @@ char *_strcat(char *dest, char *src)
 	}
 	for (j = 0 ; src[j] != '\0'; j++)
 	{
-	dest[i + j] = src[j];
+		dest[i + j] = src[j];
 	}
 	dest[i + j] = '\0';
- 	return (dest);
+	return (dest);
 }
