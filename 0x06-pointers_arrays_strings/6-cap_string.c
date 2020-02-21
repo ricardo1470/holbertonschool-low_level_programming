7d443378f5697109e449428a242a2875a5446eca
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-21 05:24:57 +0000

    function that capitalizes all words of a string, learning c

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * cap_string - Entry point
+ * Desc: Entry
+ *@a: pointer
+ * Return: Always 0 (Success)
+ */
+char *cap_string(char *a)
+{
+	int i;
+	int j;
+	char k[] = " \t\n,;.!\?\"(){}";
+
+	for (i = 0 ; a[i] != '\0'; i++)
+	{
+		for (j = 0 ; k[j] != '\0'; j++)
+		{
+			if (a[i] == k[j])
+			{
+				if (a[i + 1] >= 97 && a[i + 1] <= 122)
+				{
+					a[i + 1] = a[i + 1] - 32;
+				}
+				else if (i == 0)
+				{
+					if (a[i] > 97 && a[i + 1] <= 122)
+					{
+						a[i] = a[i] - 32;
+					}
+				}
+			}
+		}
+	}
+	return (a);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -9,5 +9,6 @@ char *_strncpy(char *dest, char *src, int n);/*prototype function that copies a 
 int _strcmp(char *s1, char *s2);/*prototype function that compares two strings.*/
 void reverse_array(int *a, int n);/*prototype  function that reverses the content of an array of integers.*/
 char *string_toupper(char *);/*prototype function that changes all lowercase letters of a string to uppercase.*/
+char *cap_string(char *);/*prototyope function that capitalizes all words of a string.*/
 
 #endif /* HOLBERTON_H */
