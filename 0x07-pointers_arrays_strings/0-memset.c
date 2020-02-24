5b1ccff7a2ab04d96baa52e107e2050eabdc1fdf
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-24 21:53:56 +0000

    function that fills memory with a constant byte. learning c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * _memset - Entry point
+ * Desc: Entry
+ *@s: pointer
+ *@b: string
+ *@n: number the bytes
+ * Return: Always 0 (Success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0 ; i <= n ; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
