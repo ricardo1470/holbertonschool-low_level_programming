1afce03e85ebdbc37b79e535ea2bf3ecb780d42f
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-24 21:54:40 +0000

    prototype

---
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+char *_memset(char *s, char b, unsigned int n);/*prototype function that fills memory with a constant byte.*/
+
+#endif /* HOLBERTON_H */
