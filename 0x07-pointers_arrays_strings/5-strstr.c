7433443554bf78d0df6f1718f3f9e426e4230637
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-25 04:23:29 +0000

    function that locates a substring. learning c rev6

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -15,16 +15,16 @@ char *_strstr(char *haystack, char *needle)
 
 	for (i = 0 ; haystack[i] ; i++)
 	{
-		for (k = i, j = 0 ; needle[j] ; j++, k++)
+		for (k = i, j = 0 ; needle[j] != '\0' ; j++, k++)
 		{
 			if (haystack[k] != needle[j] || haystack[k] == '\0')
 			{
 				break;
 			}
-			if (needle[j] == 0)
-			{
+		}
+		if (needle[j] == 0)
+		{
 			return (haystack + i);
-			}
 		}
 	}
 	return (0);
