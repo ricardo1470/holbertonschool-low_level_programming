86158be272cac37f1dfc8f7b1b18aa0335b4a63e
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-25 04:52:17 +0000

    function that locates a character in a string. learning c

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -2,8 +2,13 @@
 #define HOLBERTON_H
 #include <stdio.h>
 
+int _putchar(char c);/*_putchar*/
 char *_memset(char *s, char b, unsigned int n);/*prototype function that fills memory with a constant byte.*/
 char *_memcpy(char *dest, char *src, unsigned int n);/*function that copies memory area.*/
 char *_strchr(char *s, char c);/*function that locates a character in a string.*/
+unsigned int _strspn(char *s, char *accept);/*prototype function that gets the length of a prefix substring.*/
+char *_strpbrk(char *s, char *accept);/*function that searches a string for any of a set of bytes.*/
+char *_strstr(char *haystack, char *needle);/*function that locates a substring.*/
+void print_chessboard(char (*a)[8]);/*function that prints the chessboard.*/
 
 #endif /* HOLBERTON_H */
