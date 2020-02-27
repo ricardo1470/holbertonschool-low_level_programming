40e0141b6cea617a1fe50b49d85915363b72fa7b
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-27 00:34:09 +0000

    Recursion

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -6,5 +6,7 @@ int _putchar(char c);/*prototype _putchar*/
 void _puts_recursion(char *s);/*prototype function that prints a string, followed by a new line.*/
 void _print_rev_recursion(char *s);/*prototype function that prints a string in reverse.*/
 int _strlen_recursion(char *s);/*prototype function that returns the length of a string.*/
+int factorial(int n);/*prototype function that returns the factorial of a given number.*/
+int _pow_recursion(int x, int y);/* function that returns the value of x raised to the power of y.*/
 
 #endif /* HOLBERTON_H */
