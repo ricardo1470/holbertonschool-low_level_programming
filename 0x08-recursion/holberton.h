49a5642481b691383c09aac29ed6f5fc4eddb227
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-02-27 04:13:20 +0000

    Recursion

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -8,5 +8,8 @@ void _print_rev_recursion(char *s);/*prototype function that prints a string in 
 int _strlen_recursion(char *s);/*prototype function that returns the length of a string.*/
 int factorial(int n);/*prototype function that returns the factorial of a given number.*/
 int _pow_recursion(int x, int y);/* function that returns the value of x raised to the power of y.*/
-
+int _sqrt_recursion(int n);/*function that returns the natural square root of a number.*/
+int _my_sqrt(int j, int i);
+int is_prime_number(int n);/*function that returns 1 if the input integer is a prime number, otherwise return 0.*/
+int _my_number(int k, int l);
 #endif /* HOLBERTON_H */
