637545fa29d3a4b2a2b7f13d7c9deabe725c0142
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-01 22:39:22 +0000

    program that prints its name, learning c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -  print name
+ *@argc: int.
+ *@argv: char.
+ * Return: Always 0.
+ */
+int main(int argc, char* argv[])
+{
+	int i;
+	
+	for (i = 0; i < argc ; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+int _putchar(char c);/*prototype _putchar*/
+
+#endif /* HOLBERTON_H */
