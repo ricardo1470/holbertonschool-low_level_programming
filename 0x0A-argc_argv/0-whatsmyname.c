5f31606eeec762f47d55543a2d7a5b13c8384cca
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-01 22:43:25 +0000

    program that prints its name, learning c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,10 @@
  *@argv: char.
  * Return: Always 0.
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; i < argc ; i++)
 	{
 		printf("%s\n", argv[i]);
