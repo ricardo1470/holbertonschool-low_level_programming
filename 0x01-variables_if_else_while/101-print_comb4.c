eecd8aca1e535b06ae825c218b860d455256be12
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-02 06:35:24 +0000

    program that prints all possible different combinations of three digits. learning c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,9 +14,9 @@ int main(void)
 
 	for (i = 0; i < 1000; i++)
 	{
-		j = i / 100; /* hundreds */
-		k = (i / 10) % 10; /* tens */
-		l = i % 10; /* singles */
+		j = i / 100;
+		k = (i / 10) % 10;
+		l = i % 10;
 
 		if (j < k && k < l)
 		{
