a638df7aecb970b89ab79115282e4fbcd3401cb9
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-04 02:58:05 +0000

    prototype

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+int _putchar(char c);/*prototype _putchar*/
+char *create_array(unsigned int size, char c);/*prototype function that creates an array of chars, and initializes it with a specific char.*/
+
+#endif /* HOLBERTON_H */
