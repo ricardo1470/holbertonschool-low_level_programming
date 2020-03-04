8a618acbdc81624f426cfe5982b59f88e991154f
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-04 04:54:25 +0000

    prototype

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -5,5 +5,6 @@
 int _putchar(char c);/*prototype _putchar*/
 char *create_array(unsigned int size, char c);/*prototype function that creates an array of chars, and initializes it with a specific char.*/
 char *_strdup(char *str);/*function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.*/
+char *str_concat(char *s1, char *s2);/*prototype function that concatenates two strings*/
 
 #endif /* HOLBERTON_H */
