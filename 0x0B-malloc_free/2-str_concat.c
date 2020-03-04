f81010177173cece2e7cfaad57c929d1fdba42de
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-04 05:20:19 +0000

    function that concatenates two strings. learning c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,9 +11,12 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k, l;
 	char *p;
 	
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL) 
 	{
 		s1 = "";
+	}
+	if (s2 == NULL)
+	{
 		s2 = "";
 	}
 	for (i = 0; s1[i] != '\0'; i++)
