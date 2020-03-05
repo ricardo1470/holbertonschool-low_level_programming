237d096498d4e46f1cd4c786ae2eb75276712513
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-05 05:11:39 +0000

    prototype

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -7,5 +7,6 @@ char *create_array(unsigned int size, char c);/*prototype function that creates 
 char *_strdup(char *str);/*function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter.*/
 char *str_concat(char *s1, char *s2);/*prototype function that concatenates two strings*/
 int **alloc_grid(int width, int height);/*prototype function that returns a pointer to a 2 dimensional array of integers.*/
+void free_grid(int **grid, int height);/*prototype function that frees a 2 dimensional grid previously created by your alloc_grid function.*/
 
 #endif /* HOLBERTON_H */
