0a3c2b766e5c55797faa7c3545a3abaabf33cf8f
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-06 02:29:28 +0000

    update

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -4,6 +4,7 @@
 
 int _putchar(char c);/*prototype _putchar*/
 void *malloc_checked(unsigned int b);/*prototype function that allocates memory using malloc.*/
-char *string_nconcat(char *s1, char *s2, unsigned int n);/*function that concatenates two strings.*/
+char *string_nconcat(char *s1, char *s2, unsigned int n);/*prtotype function that concatenates two strings.*/
+void *_calloc(unsigned int nmemb, unsigned int size);/*prototype function that allocates memory for an array, using malloc.*/
 
 #endif /* HOLBERTON_H */
