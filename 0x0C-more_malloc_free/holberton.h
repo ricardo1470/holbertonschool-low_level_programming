e09294784654f5be9ac4712289abdb73f29a7b54
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-06 03:24:45 +0000

    prototype

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -6,5 +6,6 @@ int _putchar(char c);/*prototype _putchar*/
 void *malloc_checked(unsigned int b);/*prototype function that allocates memory using malloc.*/
 char *string_nconcat(char *s1, char *s2, unsigned int n);/*prtotype function that concatenates two strings.*/
 void *_calloc(unsigned int nmemb, unsigned int size);/*prototype function that allocates memory for an array, using malloc.*/
+int *array_range(int min, int max);/*prototype function that creates an array of integers.*/
 
 #endif /* HOLBERTON_H */
