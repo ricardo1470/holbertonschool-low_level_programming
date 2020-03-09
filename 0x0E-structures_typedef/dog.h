4177e41ede630de574aea126eed6df21b832d175
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-09 20:58:03 +0000

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,9 +14,12 @@ struct dog
 	float age;
 	char *owner;
 };
-
-/* Prototypes */
-int _putchar(char c);/*prototype _putchar*/
+/**
+  * _putchar - print a character
+  * @c: character.
+  * Return: c
+  */
+int _putchar(char c); /*prototype _putchar*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 /*function that initialize a variable of type struct dog*/
 
