0f2b0c1ecf35893164ce100838eb835937e55818
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-09 20:14:42 +0000

    Define a new type struct dog, learning c

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,5 +15,6 @@ struct dog
 	char *owner;
 };
 int _putchar(char c);/*prototype _putchar*/
-void init_dog(struct dog *d, char *name, float age, char *owner);/*function that initialize a variable of type struct dog*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+/*function that initialize a variable of type struct dog*/
 #endif /* HOLBERTON_H */
