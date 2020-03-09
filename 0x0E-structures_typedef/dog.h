75bb98fd6bd77750efe4a93f000e883fa067aa18
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-09 23:18:25 +0000

    Fix checks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,6 +22,11 @@ struct dog
 int _putchar(char c); /*prototype _putchar*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 /*function that initialize a variable of type struct dog*/
+/**
+  * print_dog - print a character
+  * @d: character.
+  * Return: c
+  */
 void print_dog(struct dog *d);/*function that prints a struct dog*/
 
 #endif /* HOLBERTON_H */
--- 0x0E-structures_typedef/holberton.h
+++
@@ -1,6 +0,0 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-
-int _putchar(char c);/*prototype _putchar*/
-
-#endif /* HOLBERTON_H */
