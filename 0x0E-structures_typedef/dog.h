1a885bca92ccb9e658eb8007e97b0c478d9410e8
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-09 19:10:50 +0000

    Define a new type struct dog, learning c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/**
+ * struct dog - Short description
+ * @name: firts member
+ * @age: second member
+ * @owner: third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* HOLBERTON_H */
