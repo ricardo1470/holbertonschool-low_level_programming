488dad8a28f7dbeda36ad64881b9e1dab3adfba6
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-09 20:08:57 +0000

    function that initialize a variable, learning c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog -  initializes a variable
+ * @d: struct dog.
+ * @name: firts member name
+ * @age: second member age
+ * @owner: third member owner
+ *
+ * Description: Longer description
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
