bc61334eebc631e94e04e1c05045b851ebe9d02d
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-10 01:57:51 +0000

    function that frees dogs.

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+  * free_dog - print a character
+  * @d: firts member
+  * Description: Longer description
+  */
+void free_dog(dog_t *d)
+{
+	if(d != NULL)
+	{
+		free (d->name);
+		free (d->owner);
+		free (d);
+	}
+}
