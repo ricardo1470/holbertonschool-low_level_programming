070d34b6ba4f11bfbdcb20ea0cd6a1ec6bc266aa
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-11 23:12:13 +0000

    function that executes a function, learning c rev1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (size <= 0 || action == NULL)
+	if (array == NULL || size <= 0 || action == NULL)
 	{
 		return;
 	}
