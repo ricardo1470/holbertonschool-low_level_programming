3f50dfac6e57d1e87c3d03bab2d53c37705d4321
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-11 20:51:42 +0000

    prints a name. learnig c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include "function_pointers-h"
+#include <sdtlib.h>
+
+/**
+  * print_name - initializes a variable
+  *
+  * @name: name.
+  * @f: pointer.
+  * Return: nothing.
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+	{
+		return;
+	}
+	if (f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
