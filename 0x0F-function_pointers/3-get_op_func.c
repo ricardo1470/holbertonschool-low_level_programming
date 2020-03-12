e443e01224a8060741bf3108abbffed568948fe3
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-12 05:16:12 +0000

    calc

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <string.h>
+#include <stdlib.h>
 /**
   * get_op_func - selects the correct function to perform the operatio
   * nasked by the user.
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
   * op_add - add of two numbers.
