eef1622d0fa6fc4fb6f18236d4eb5381edca9e6e
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-12 04:36:24 +0000

    calc

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "calc.h"
+
+/**
+  * op_add - initializes a variable
+  *
+  * @a: int.
+  * @b: int.
+  * Return: nothing.
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+  * op_sub - initializes a variable
+  *
+  * @a: int.
+  * @b: int.
+  * Return: nothing.
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+  * op_mul - initializes a variable
+  *
+  * @a: int.
+  * @b: int.
+  * Return: nothing.
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+  * op_div - initializes a variable
+  *
+  * @a: int.
+  * @b: int.
+  * Return: nothing.
+  */
+int op_div(int a, int b)
+{
+	if (b != 0)
+	{
+		return (a / b);
+	}
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+  * op_mod - initializes a variable
+  *
+  * @a: int.
+  * @b: int.
+  * Return: nothing.
+  */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	return (a % b);
+}
