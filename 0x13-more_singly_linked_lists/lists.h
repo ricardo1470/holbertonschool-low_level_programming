a087972bd8bc2c257bc7bf94eacac3f1f8b7a752
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-24 09:02:54 +0000

    update prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -24,10 +24,20 @@ int _putchar(char c);
 size_t print_listint(const listint_t *h);
 /*prototype function that prints all the elements of a listint_t list.*/
 size_t listint_len(const listint_t *h);
-/*prototype function that returns the number of elements in a linked listint_t list.*/
+/**
+ * prototype function that returns the number of elements
+ * in a linked listint_t list.
+ */
 listint_t *add_nodeint(listint_t **head, const int n);
-/*prototype function that adds a new node at the beginning of a listint_t list.*/
+/**
+ * prototype function that adds a new node at the beginning
+ * of a listint_t list.
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n);
-/*prototype function that adds a new node at the end of a listint_t list.*/
+/**
+ * prototype function that adds a new node
+ * at the end of a listint_t list.
+ */
+
 
 #endif /* LISTS_H */
