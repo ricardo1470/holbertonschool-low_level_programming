6cc5e55450b02f601157d29b05dbc2aa1810f334
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-25 04:11:23 +0000

    update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -38,6 +38,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 /*prototype function that returns the sum of all the data (n) of a listint_t.*/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
-/*function that inserts a new node at a given position.*/
+/*prototype function that inserts a new node at a given position.*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+/*prototy pefunction that deletes the node at index index of a listint_t.*/
 
 #endif /* LISTS_H */
