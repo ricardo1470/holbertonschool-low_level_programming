cc5be5a5f246caec96e338846a4b3f6123389818
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-26 21:33:44 +0000

    prototype

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int _putchar(char c);
+/*prototype _putchar*/
+unsigned int binary_to_uint(const char *b);
+/*function that converts a binary number to an unsigned int.*/
+int _pow_recursion(int x, int y);
+#endif /* HOLBERTON_H */
