a824925674a0a7788559b11c89a14cccb807412f
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-27 01:36:00 +0000

    update

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -18,5 +18,7 @@ int get_bit(unsigned long int n, unsigned int index);
 /*function that returns the value of a bit at a given index.*/
 int set_bit(unsigned long int *n, unsigned int index);
 /* function that sets the value of a bit to 1 at a given index.*/
+int clear_bit(unsigned long int *n, unsigned int index);
+/*function that sets the value of a bit to 0 at a given index.*/
 
 #endif /* HOLBERTON_H */
