a15cfe133e66fbaf78fa741d168083050716ed0d
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-27 00:56:34 +0000

    function that prints the binary representation of a number.

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Entry point.
+ * @b:pointer value.
+ * Return: Always 0 (Success)
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	bin_print(n);
+}
+/**
+ * bin_print - prints the binary in reverse
+ * @n: base 10 number
+ */
+void bin_print(unsigned long int n)
+{
+	if (n == 0)
+	{
+		return;
+	}
+	else
+	{
+		bin_print(n >> 1);
+	}
+	_putchar((n & 1) + '0');
+}
