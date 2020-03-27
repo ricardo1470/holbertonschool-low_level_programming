2bc2acd2bfb443dc90a31db0358b4be4182a50d6
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-27 01:46:06 +0000

    update

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -20,5 +20,7 @@ int set_bit(unsigned long int *n, unsigned int index);
 /* function that sets the value of a bit to 1 at a given index.*/
 int clear_bit(unsigned long int *n, unsigned int index);
 /*function that sets the value of a bit to 0 at a given index.*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+/* function that returns the number of bits*/
 
 #endif /* HOLBERTON_H */
