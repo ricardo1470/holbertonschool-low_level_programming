c0f67db87eb8e743ae15a672d499d7b5348aa544
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-03-27 01:16:02 +0000

    función que establezca el valor de un bit 1en un índice dado.

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * set_bit - Entry point.
+ * @index:pointer value.
+ * @n: int value.
+ * Return: Always 0 (Success)
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if(index < 64)
+	{
+		*n += 1 << index;
+		return (1);
+	}
+	return (-1);
+}
