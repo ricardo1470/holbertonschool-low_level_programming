4f3235d2cd34c1dc50046252fc8ded7231cab3b8
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-04-01 02:20:56 +0000

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,28 +8,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i, j, k;
+	int i, j, k = 0;
 
-	k = 0;
 	if (!filename)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		return (1);
-	}
 	i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (i < 0)
+	if (k == -1)
 	{
 		return (-1);
 	}
-	while (text_content[k])
+	if (text_content)
 	{
-		k++;
+		while (text_content[k] != '\0')
+		{
+			k++;
+		}
+		j = write(i, text_content, k);
 	}
-	j = write(i, text_content, k);
-	if (j < 0)
+	if (j == -1 || j != k)
 	{
 		close(i);
 		return (-1);
