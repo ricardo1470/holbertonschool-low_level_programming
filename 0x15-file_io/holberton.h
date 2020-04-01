71592d80bb5eb41978eb27330939a09c2dac3d55
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-04-01 02:57:22 +0000

    update prototype

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -14,5 +14,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 /*unction that reads a text file and prints it to the POSIX standard output.*/
 int create_file(const char *filename, char *text_content);
 /*function that creates a file.*/
+int append_text_to_file(const char *filename, char *text_content);
+/*function that appends text at the end of a file.*/
 
 #endif /* HOLBERTON_H */
