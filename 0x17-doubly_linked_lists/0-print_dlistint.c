a0c1d7edc84c30e9f0ea57fd9ffd1c9c81458c76
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-02 18:57:36 +0000

    double linked list, earning c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Entry point.
+ * @h:value.
+ * Return: Always 0 (Success)
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t a;
+	dlistint_t *list;
+
+	list = (dlistint_t *)h;
+
+	for (a = 0; list != NULL; a++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (a);
+}
