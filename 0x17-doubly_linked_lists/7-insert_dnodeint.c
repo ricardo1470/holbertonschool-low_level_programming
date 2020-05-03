3d5d64bfb74c7679ddb54d1a8b7e18f2652c4e9a
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-03 23:21:22 +0000

    double linked lists, learnin c

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - Entry point.
+ * @head:pointer value.
+ * @idx: unsigned int value.
+ * @n: int value
+ * Return: Always 0 (Success)
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *node, *tmp = *h;
+	unsigned int count = 0;
+
+	node = malloc(sizeof(dlistint_t));
+	if (!node)
+		return (NULL);
+	node->n = n;
+	if (idx == 0)
+	{
+		node->next = tmp;
+		*h = node;
+		return (node);
+	}
+	while (count < (idx - 1))
+	{
+		if (!tmp)
+		{
+			free(node);
+			return (NULL);
+		}
+		count++;
+		tmp = tmp->next;
+	}
+	node->next = tmp->next;
+	tmp->next = node;
+	return (node);
+}
