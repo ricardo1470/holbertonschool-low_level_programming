a39e765358fb57a1d33bb260a0f7255adce07d2c
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-03 23:34:21 +0000

    double linked lists, learnin c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -16,23 +16,28 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!node)
 		return (NULL);
 	node->n = n;
+	if (!tmp && idx > 0)
+		free(node);
+		return (NULL);
 	if (idx == 0)
-	{
-		node->next = tmp;
+		if (!tmp)
+			node->next = NULL;
+		else
+			node->next = tmp;
+			tmp->prev = node;
+		node->prev = NULL;
 		*h = node;
 		return (node);
-	}
 	while (count < (idx - 1))
-	{
 		if (!tmp)
-		{
 			free(node);
 			return (NULL);
-		}
 		count++;
 		tmp = tmp->next;
-	}
 	node->next = tmp->next;
+	node->prev = tmp;
+	if (tmp->next)
+		tmp->next->prev = node;
 	tmp->next = node;
 	return (node);
 }
