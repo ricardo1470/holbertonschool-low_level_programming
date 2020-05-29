e30c969d70870a5c1a1cdedcb87197ab15240e50
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-29 21:53:13 +0000

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -17,7 +17,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
-		free(table);
 		return (NULL);
 	}
 	table->array = malloc(sizeof(hash_node_t *) * size);
