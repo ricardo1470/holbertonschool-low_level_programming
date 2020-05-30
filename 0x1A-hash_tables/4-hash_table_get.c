02777e9a43a27b19d6b15001a1e4925a20abc497
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-05-30 01:11:03 +0000

    create

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - implementation of the djb2 algorithm
+ * @ht: value pointer
+ * @key: value
+ * @value: value
+ * Return: hash value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+
