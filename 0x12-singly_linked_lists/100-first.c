9dd855b3a8e29eddd07d9b24c683597339569e22
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-07-23 21:53:06 -0500

    update prototype

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,9 +3,9 @@
 /**
  * print_afert_main - before the main function is executed.
  **/
-void print_afert_main(void) __attribute__((constructor));
+void print_after_main(void) __attribute__((constructor));
 
-void print_afert_main(void)
+void print_after_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
