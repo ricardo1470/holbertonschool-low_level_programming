6128374dd9da21f0627c9d68445aaa1d78d2da08
ricardo1470 <ricardo.alfonso.camayo@gmail.com>
2020-08-27 05:14:18 -0500

    update file
    function that searches
    
    for a value in an array
    
    of integers using
    
    the Linear search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,21 +10,19 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	int i;
 
 	if (!array)
 	{
 		return (-1);
 	}
-	if (array != NULL)
+
+	for (i = 0; i < (int)size ; i++)
 	{
-		for (i = 0; i < size ; i++)
+		printf("Value checked array [%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
 		{
-			printf("Value checked array [%lu] = [%d]\n", i, array[i]);
-			if (array[i] == value)
-			{
-				return (i);
-			}
+			return (i);
 		}
 	}
 	return (-1);
